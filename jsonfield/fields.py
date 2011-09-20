556941c04ac16eb39c4fd39c651fbbf40b12332c
Brad Jasper <bjasper@gmail.com>
2011-09-20 14:36:03 -0400

    removed connection parameter from get_db_prep_save

--- jsonfield/fields.py
+++ jsonfield/fields.py
@@ -23,7 +23,7 @@ class JSONField(models.TextField):
 
         return value
 
-    def get_db_prep_save(self, value, connection):
+    def get_db_prep_save(self, value):
         """Convert our JSON object to a string before we save"""
 
         if not value or value == "":
@@ -32,7 +32,7 @@ class JSONField(models.TextField):
         if isinstance(value, (dict, list)):
             value = json.dumps(value, cls=DjangoJSONEncoder)
 
-        return super(JSONField, self).get_db_prep_save(value, connection)
+        return super(JSONField, self).get_db_prep_save(value)
 
 try:
     from south.modelsinspector import add_introspection_rules
