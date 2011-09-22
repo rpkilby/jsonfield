0074a18bf41dd5fadc877900f0c02cb98920f8d0
Anton <krontab@ymail.com>
2011-09-22 12:42:25 +0200

    Fixed get_db_prep_save() error

--- jsonfield/fields.py
+++ jsonfield/fields.py
@@ -32,7 +32,7 @@ class JSONField(models.TextField):
         if isinstance(value, (dict, list)):
             value = json.dumps(value, cls=DjangoJSONEncoder)
 
-        return super(JSONField, self).get_db_prep_save(value, connection)
+        return super(JSONField, self).get_db_prep_save(value, connection=connection)
 
 try:
     from south.modelsinspector import add_introspection_rules
