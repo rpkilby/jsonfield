f178bafaa6cc3161150f78c561ea0643cdf50cea
Nicholas Serra <nickserra@gmail.com>
2012-01-14 12:57:41 -0500

    Fixed django admin bug, added django admin validaiton
    Fixed the bug in the django admin that saved the field with invalid
    json. Added a formfield method and Form Field to validate that valid
    json is being entered in the django admin before it is saved. Throws
    django admin form error for bad json.

--- jsonfield/fields.py
+++ jsonfield/fields.py
@@ -1,7 +1,21 @@
 from django.db import models
 from django.core.serializers.json import DjangoJSONEncoder
+from django.core.exceptions import ValidationError
 from django.utils import simplejson as json
+from django.utils.translation import ugettext_lazy as _
 
+from django.forms.fields import Field
+from django.forms.util import ValidationError as FormValidationError
+
+class JSONFormField(Field):
+    def clean(self, value):
+        value = super(JSONFormField, self).clean(value)
+
+        try:
+            json.loads(value)
+        except ValueError:
+            raise FormValidationError(_("Enter valid JSON"))
+        return value
 
 class JSONField(models.TextField):
     """JSONField is a generic textfield that serializes/unserializes JSON objects"""
@@ -17,26 +31,31 @@ class JSONField(models.TextField):
 
     def to_python(self, value):
         """Convert string value to JSON"""
-
         if isinstance(value, basestring):
             try:
                 return json.loads(value, **self.load_kwargs)
             except ValueError:
-                pass
-
+                raise exceptions.ValidationError(_("Enter valid JSON"))
         return value
 
-    def get_db_prep_value(self, value):
+    def get_db_prep_value(self, value, connection, prepared=False):
         """Convert JSON object to a string"""
 
         if isinstance(value, basestring):
             return value
-
         return json.dumps(value, **self.dump_kwargs)
 
     def value_to_string(self, obj):
         value = self._get_val_from_obj(obj)
         return self.get_db_prep_value(value)
+
+    def value_from_object(self, obj):
+        return json.dumps(super(JSONField, self).value_from_object(obj))
+        
+    def formfield(self, form_class=JSONFormField, **kwargs):
+        defaults = {"help_text": "Enter valid JSON"}
+        defaults.update(kwargs)
+        return form_class(**defaults)
 
 try:
     from south.modelsinspector import add_introspection_rules
