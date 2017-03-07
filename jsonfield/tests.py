98189fc42deea252c8682405ddcba1ffadac37a3
Daniel Koch <dkoch@mm.st>
2017-03-07 14:19:26 -0500

    Renamed method get_db_prep_value to get_prep_value
    Reformat of PR #152, closes issue #151

--- jsonfield/tests.py
+++ jsonfield/tests.py
@@ -16,6 +16,8 @@ try:
 except ImportError:
     from django.forms.util import ValidationError
 
+from django.utils.six import string_types
+
 from collections import OrderedDict
 
 
@@ -309,3 +311,62 @@ class JsonModelFormTest(TestCase):
     def test_form_with_data(self):
         form = JsonNotRequiredForm(data={'json': '{}'})
         self.assertTrue(form.has_changed())
+
+
+class TestFieldAPIMethods(TestCase):
+    def test_get_db_prep_value_method_with_null(self):
+        json_field_instance = JSONField(null=True)
+        value = {'a': 1}
+        prepared_value = json_field_instance.get_db_prep_value(
+            value, connection=None, prepared=False)
+        self.assertIsInstance(prepared_value, string_types)
+        self.assertDictEqual(value, json.loads(prepared_value))
+        self.assertIs(json_field_instance.get_db_prep_value(
+            None, connection=None, prepared=True), None)
+        self.assertIs(json_field_instance.get_db_prep_value(
+            None, connection=None, prepared=False), None)
+
+    def test_get_db_prep_value_method_with_not_null(self):
+        json_field_instance = JSONField(null=False)
+        value = {'a': 1}
+        prepared_value = json_field_instance.get_db_prep_value(
+            value, connection=None, prepared=False)
+        self.assertIsInstance(prepared_value, string_types)
+        self.assertDictEqual(value, json.loads(prepared_value))
+        self.assertIs(json_field_instance.get_db_prep_value(
+            None, connection=None, prepared=True), None)
+        self.assertEqual(json_field_instance.get_db_prep_value(
+            None, connection=None, prepared=False), 'null')
+
+    def test_get_db_prep_value_method_skips_prepared_values(self):
+        json_field_instance = JSONField(null=False)
+        value = {'a': 1}
+        prepared_value = json_field_instance.get_db_prep_value(
+            value, connection=None, prepared=True)
+        self.assertIs(prepared_value, value)
+
+    def test_get_prep_value_always_json_dumps_if_not_null(self):
+        json_field_instance = JSONField(null=False)
+        value = {'a': 1}
+        prepared_value = json_field_instance.get_prep_value(value)
+        self.assertIsInstance(prepared_value, string_types)
+        self.assertDictEqual(value, json.loads(prepared_value))
+        already_json = json.dumps(value)
+        double_prepared_value = json_field_instance.get_prep_value(
+            already_json)
+        self.assertDictEqual(value,
+                             json.loads(json.loads(double_prepared_value)))
+        self.assertEqual(json_field_instance.get_prep_value(None), 'null')
+
+    def test_get_prep_value_can_return_none_if_null(self):
+        json_field_instance = JSONField(null=True)
+        value = {'a': 1}
+        prepared_value = json_field_instance.get_prep_value(value)
+        self.assertIsInstance(prepared_value, string_types)
+        self.assertDictEqual(value, json.loads(prepared_value))
+        already_json = json.dumps(value)
+        double_prepared_value = json_field_instance.get_prep_value(
+            already_json)
+        self.assertDictEqual(value,
+                             json.loads(json.loads(double_prepared_value)))
+        self.assertIs(json_field_instance.get_prep_value(None), None)
