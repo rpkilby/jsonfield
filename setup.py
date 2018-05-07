8ce228e59f664f833bb098a6b8232f5a6c952daa
Ryan P Kilby <rpkilby@ncsu.edu>
2018-05-07 15:34:02 -0400

    Update distribution info

--- setup.py
+++ setup.py
@@ -1,21 +1,18 @@
-
 from setuptools import setup
 
 
 setup(
-    name='jsonfield',
+    name='jsonfield2',
     version='2.0.2',
     packages=['jsonfield'],
     license='MIT',
     include_package_data=True,
-    author='Dan Koch',
-    author_email='dmkoch@gmail.com',
-    url='https://github.com/dmkoch/django-jsonfield/',
+    author='Ryan P Kilby',
+    author_email='rpkilby@ncsu.edu',
+    url='https://github.com/rpkilby/jsonfield2/',
     description='A reusable Django field that allows you to store validated JSON in your model.',
     long_description=open("README.rst").read(),
-    install_requires=['Django >= 1.8.0'],
-    tests_require=['Django >= 1.8.0'],
-    cmdclass={'test': TestCommand},
+    install_requires=['Django >= 1.11'],
     classifiers=[
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
@@ -23,7 +20,6 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
