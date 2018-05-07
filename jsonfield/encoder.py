e133144e725b9b475e215d5f4011e9bf4270d37a
Ryan P Kilby <rpkilby@ncsu.edu>
2018-05-07 15:30:01 -0400

    Update JSONEncoder from DRF

--- jsonfield/encoder.py
+++ jsonfield/encoder.py
@@ -17,13 +17,11 @@ class JSONEncoder(json.JSONEncoder):
     """
     def default(self, obj):  # noqa
         # For Date Time string spec, see ECMA 262
-        # http://ecma-international.org/ecma-262/5.1/#sec-15.9.1.15
+        # https://ecma-international.org/ecma-262/5.1/#sec-15.9.1.15
         if isinstance(obj, Promise):
             return force_text(obj)
         elif isinstance(obj, datetime.datetime):
             representation = obj.isoformat()
-            if obj.microsecond:
-                representation = representation[:23] + representation[26:]
             if representation.endswith('+00:00'):
                 representation = representation[:-6] + 'Z'
             return representation
@@ -33,8 +31,6 @@ class JSONEncoder(json.JSONEncoder):
             if timezone and timezone.is_aware(obj):
                 raise ValueError("JSON can't represent timezone-aware times.")
             representation = obj.isoformat()
-            if obj.microsecond:
-                representation = representation[:12]
             return representation
         elif isinstance(obj, datetime.timedelta):
             return six.text_type(obj.total_seconds())
@@ -45,13 +41,16 @@ class JSONEncoder(json.JSONEncoder):
             return six.text_type(obj)
         elif isinstance(obj, QuerySet):
             return tuple(obj)
+        elif isinstance(obj, six.binary_type):
+            # Best-effort for binary blobs. See #4187.
+            return obj.decode('utf-8')
         elif hasattr(obj, 'tolist'):
             # Numpy arrays and array scalars.
             return obj.tolist()
         elif hasattr(obj, '__getitem__'):
             try:
                 return dict(obj)
-            except:
+            except Exception:
                 pass
         elif hasattr(obj, '__iter__'):
             return tuple(item for item in obj)
