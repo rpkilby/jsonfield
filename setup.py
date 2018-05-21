aed087e87abcf3e189dcca3048bf57d960e8dd0d
Ryan P Kilby <rpkilby@ncsu.edu>
2018-05-21 17:13:04 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='jsonfield2',
-    version='3.0.0',
+    version='3.0.1',
     packages=['jsonfield'],
     license='MIT',
     include_package_data=True,
