30beb147db39780477ae904ba1ba08bb84da4ff2
Krzysztof Stopa <stopa.krzysztof.k@gmail.com>
2019-12-29 23:33:52 +0100

    Updated tests with compatibility for Django 3

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     url='https://github.com/dmkoch/django-jsonfield/',
     description='A reusable Django field that allows you to store validated JSON in your model.',
     long_description=open("README.rst").read(),
-    install_requires=['Django >= 1.8.0'],
+    install_requires=['Django >= 1.8.0', 'six>=1.12'],
     tests_require=['Django >= 1.8.0'],
     cmdclass={'test': TestCommand},
     classifiers=[
@@ -44,7 +44,6 @@ setup(
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
