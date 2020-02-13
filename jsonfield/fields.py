a1d6e58e1e4a20472c416d47faf7e47ca15606d6
Ryan P Kilby <kilbyr@gmail.com>
2020-02-13 15:42:42 -0800

    Don't use unicode gettext variants

--- jsonfield/fields.py
+++ jsonfield/fields.py
@@ -3,7 +3,7 @@ import json
 
 from django.db import models
 from django.forms import ValidationError
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from . import forms
 from .encoder import JSONEncoder
--- jsonfield/forms.py
+++ jsonfield/forms.py
@@ -1,7 +1,7 @@
 import json
 
 from django.forms import ValidationError, fields
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 
 class JSONFieldMixin(object):
