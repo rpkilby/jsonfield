cb749a8e5de019c88349a184c2863429923b767f
Ryan P Kilby <kilbyr@gmail.com>
2020-02-22 00:36:44 -0800

    Bump minor version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='jsonfield',
-    version='3.0.0',
+    version='3.1.0',
     license='MIT',
     include_package_data=True,
     author='Brad Jasper',
